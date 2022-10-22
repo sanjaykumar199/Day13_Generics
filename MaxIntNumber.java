46bc9dc3276e0a3b27a85b8798f1024cddd931d1
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 20:01:59 -0500

    Day13_Generics

---
+++ MaxIntNumber.java
@@ -0,0 +1,7 @@
+package Com.bridgelab.day13_Generics;
+
+public class MaxIntNumber {
+    public static void main(String[] args) {
+        System.out.println("The max Number Among 3 numbers is: "+maxNumberInteger(93, 26, 43));
+    }
+}
