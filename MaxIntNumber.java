17aa12094873588a8400a43f70868503f7bc8097
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 20:10:25 -0500

    uc2-max-float-num

--- MaxIntNumber.java
+++ MaxIntNumber.java
@@ -1,9 +1,9 @@
 package Com.bridgelab.day13_Generics;
 
 public class MaxIntNumber {
-    public static Integer maxNumberInteger(Integer num1, Integer num2, Integer num3)
+    public static Float maxNumberFloat(Float num1, Float num2, Float num3)
     {
-        Integer max = num1;
+        Float max = num1;
         if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
             max = num1;
         else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
@@ -12,7 +12,8 @@ public class MaxIntNumber {
             max = num3;
         return max;
     }
+
     public static void main(String[] args) {
-        System.out.println("The max Number Among 3 numbers is: "+maxNumberInteger(93, 26, 43));
+        System.out.println("The max Number Among 3 numbers is: "+maxNumberFloat(93.32f, 2698.76f, 437.3f));
     }
 }
