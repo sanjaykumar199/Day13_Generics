ba3078726ac94e2b37e290589b3fcd9ad9af8a04
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-10-21 20:05:11 -0500

    uc1-max-int-number

--- MaxIntNumber.java
+++ MaxIntNumber.java
@@ -1,6 +1,17 @@
 package Com.bridgelab.day13_Generics;
 
 public class MaxIntNumber {
+    public static Integer maxNumberInteger(Integer num1, Integer num2, Integer num3)
+    {
+        Integer max = num1;
+        if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+            max = num1;
+        else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+            max = num2;
+        else
+            max = num3;
+        return max;
+    }
     public static void main(String[] args) {
         System.out.println("The max Number Among 3 numbers is: "+maxNumberInteger(93, 26, 43));
     }
